911f3ca8ba76df7fb124fcc549c49008d6492147
Joey574 <joeysoroka@outlook.com>
2025-01-15 08:49:35 -0800

    basic load distribution, just creates work when requested, next step would be to have a set of work, then distribute from that

--- Load Balancing/control.go
+++ Load Balancing/control.go
@@ -1,14 +1,51 @@
+//go:build a
+
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"math/rand/v2"
 	"net"
 	"os"
+	"time"
 )
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, id int) {
 	defer conn.Close()
+	println("Worker Connected")
 
-	println("Handling connection")
+	recvbuf := make([]byte, 4)
+
+	for {
+		a := 200 + rand.Float32()*(150)
+		b := 100 + rand.Float32()*(80)
+		c := 1 + rand.Float32()*(0.1)
+
+		buf := new(bytes.Buffer)
+		binary.Write(buf, binary.BigEndian, a)
+		binary.Write(buf, binary.BigEndian, b)
+		binary.Write(buf, binary.BigEndian, c)
+
+		startTime := time.Now()
+
+		_, err := conn.Write(buf.Bytes())
+		if err != nil {
+			break
+		}
+		_, err = conn.Read(recvbuf)
+		if err != nil {
+			break
+		}
+
+		elapsed := time.Since(startTime)
+
+		recvval := binary.BigEndian.Uint32(recvbuf)
+		res := float32(recvval)
+		print("Result (conn ", id, "): ", res, " | ", elapsed.Seconds(), " seconds\n")
+	}
+
+	println("Worker Disconnected")
 }
 
 func main() {
@@ -24,6 +61,7 @@ func main() {
 	}
 	defer listener.Close()
 
+	workers := 0
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -31,6 +69,7 @@ func main() {
 			continue
 		}
 
-		go handleConnection(conn)
+		go handleConnection(conn, workers)
+		workers += 1
 	}
 }
--- Load Balancing/worker.go
+++ Load Balancing/worker.go
@@ -1,10 +1,77 @@
+//go:build b
+
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"math"
+	"math/rand/v2"
 	"net"
 	"os"
 )
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	recvbuf := make([]byte, 12)
+
+	for {
+		_, err := conn.Read(recvbuf)
+		if err != nil {
+			break
+		}
+
+		ua := binary.BigEndian.Uint32(recvbuf[:4])
+		ub := binary.BigEndian.Uint32(recvbuf[4:8])
+		uc := binary.BigEndian.Uint32(recvbuf[8:12])
+
+		a := float32(ua)
+		b := float32(ub)
+		c := float32(uc)
+
+		res := float32(0.0)
+
+		for range 100000000 {
+			res += (a / b) * c
+
+			a *= (c / b) + a
+			b *= (b * c) / a
+			c /= b + c
+
+			res -= (a / b) * c
+
+			a /= b
+			b /= a
+
+			a *= float32(math.Sin(float64(b)))
+			b *= float32(math.Cos(float64(a)))
+			c = float32(math.Sin(float64(c)))
+
+			res *= (a / b) + a
+
+			res = float32(math.Sin(float64(res)))
+
+			a = 200 + rand.Float32()*(150)
+			b = 100 + rand.Float32()*(80)
+			c = 1 + rand.Float32()*(0.1)
+		}
+
+		res /= (res - a) / b
+		res = float32(math.Cos(float64(res)))
+
+		buf := new(bytes.Buffer)
+		binary.Write(buf, binary.BigEndian, res)
+
+		_, err = conn.Write(buf.Bytes())
+		if err != nil {
+			break
+		}
+	}
+
+	println("Host Disconnected")
+}
 
 func main() {
 	print("Worker Started\n")
@@ -19,6 +86,6 @@ func main() {
 		fmt.Println("Error Connecting:", err)
 		os.Exit(1)
 	}
-	defer conn.Close()
 
+	handleConnection(conn)
 }
